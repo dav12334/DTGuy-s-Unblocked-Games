60cc2143856662b73667b5e7ca815b40ab1798f2
David <92689808+dav12334@users.noreply.github.com>
2025-05-08 08:41:44 -0400

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
 var sitename = "DTGuy's Unblocked Games"; // Change this to change the name of your website.
-var subtext = "v1.2"; // set the subtext
+var subtext = "HIIIII"; // set the subtext
 
 // more settings in main.css
 
